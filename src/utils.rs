5fa20a5819d6eccdb20e74b3a0f3717e168c600c
πa <76558220+rkdud007@users.noreply.github.com>
2025-08-04 17:36:38 +0100

    random_bgg_encodings

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,4 @@
+use keccak_asm::Keccak256;
 use memory_stats::memory_stats;
 use std::{
     env,
@@ -6,8 +7,15 @@ use std::{
 use tracing::{debug, info};
 
 use crate::{
+    bgg::{
+        encoding::BggEncoding,
+        sampler::{BGGEncodingSampler, BGGPublicKeySampler},
+    },
+    matrix::base::BaseMatrix,
     poly::dcrt::{params::DCRTPolyParams, poly::DCRTPoly},
-    sampler::{DistType, PolyUniformSampler, uniform::DCRTPolyUniformSampler},
+    sampler::{
+        DistType, PolyUniformSampler, hash::DCRTPolyHashSampler, uniform::DCRTPolyUniformSampler,
+    },
 };
 
 /// ideal thread chunk size for parallel
@@ -104,6 +112,31 @@ pub fn create_random_poly(params: &DCRTPolyParams) -> DCRTPoly {
 pub fn create_bit_random_poly(params: &DCRTPolyParams) -> DCRTPoly {
     let sampler = DCRTPolyUniformSampler::new();
     sampler.sample_poly(params, &DistType::BitDist)
+}
+
+pub fn random_bgg_encodings(
+    input_size: usize,
+    secret_size: usize,
+    params: &DCRTPolyParams,
+) -> Vec<BggEncoding<BaseMatrix<DCRTPoly>>> {
+    // Create samplers
+    let key: [u8; 32] = rand::random();
+    let bgg_pubkey_sampler =
+        BGGPublicKeySampler::<_, DCRTPolyHashSampler<Keccak256>>::new(key, secret_size);
+    let uniform_sampler = DCRTPolyUniformSampler::new();
+
+    // Generate random tag for sampling
+    let tag: u64 = rand::random();
+    let tag_bytes = tag.to_le_bytes();
+    // Create secret and plaintexts
+    let secrets = vec![create_bit_random_poly(params); secret_size];
+    let plaintexts = vec![create_random_poly(params); input_size];
+
+    // Create random public keys
+    let reveal_plaintexts = vec![true; input_size + 1];
+    let bgg_encoding_sampler = BGGEncodingSampler::new(params, &secrets, uniform_sampler, 0.0);
+    let pubkeys = bgg_pubkey_sampler.sample(params, &tag_bytes, &reveal_plaintexts);
+    bgg_encoding_sampler.sample(params, &pubkeys, &plaintexts)
 }
 
 pub fn timed_read<T, F: FnOnce() -> T>(label: &str, f: F, total: &mut Duration) -> T {
